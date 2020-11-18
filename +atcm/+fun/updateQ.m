5fb07f30a0a0da5c3e0919beb186c8cd89a65281
Alex <alexandershaw4@gmail.com>
2020-11-18 15:56:50 +0000

    +

--- +atcm/+fun/updateQ.m
+++ +atcm/+fun/updateQ.m
@@ -1,14 +1,16 @@
 function DCM = updateQ(DCM)
 
 y = DCM.xY.y;
-w = DCM.xY.Hz;
+w = linspace(1,2,length(DCM.xY.Hz));
 
 for i = 1:length(y)
     n      = size(y{i},1);
     m      = size(y{i},2)*size(y{i},3);
     q      = spm_Q(1/2,n,1);
-    q      = diag( .25+(w/w(end)) ) .* q;
-    q      = q.*hamming(length(w));
+    q      = q.*~eye(length(q));
+    
+    q      = diag( w ) + q;
+    %q      = q.*hamming(length(w));
     Q{i,i} = kron(speye(m,m),q);
 end
 DCM.xY.Q  = spm_cat(Q);
