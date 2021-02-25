83eccb6faf118b1109ca7134051da4a89bab028a
Alex <alexandershaw4@gmail.com>
2021-02-25 12:01:26 +0000

    added laplacian smoothing

--- +atcm/+fun/FastFreqInt.m
+++ +atcm/+fun/FastFreqInt.m
@@ -7,25 +7,32 @@ M.x   = spm_dcm_neural_x(P,M);
 
 [f,dfdx,D] = feval(M.f,M.x,M.u,P,M);
 
+dfdx  = D*dfdx;
 [v,s] = eig(full(dfdx),'nobalance');
 s     = diag(s);
+
 dfdu  = spm_diff(M.f,M.x,M.u,P,M,2);
+dfdu  = D*dfdu;
 dvdu  = pinv(v)*dfdu;
 
-S = 0;
-for k = 1:length(s)
-    Sk = 1./(1j*2*pi*w - s(k));
-    S  = S + dvdu(k,:)*Sk;
-end
+
+% use makef to position Xhz oscillations in frequency space 
+S = atcm.fun.makef(w,s,dvdu,s*0+4);
+ 
+
+% for k = 1:length(s)
+%     Sk = 1./(1j*2*pi*w - s(k));
+%     S  = S + dvdu(k,:)*Sk;
+% end
 
 Pf = S(:);
 
-[Gu,Gs,Gn] = spm_csd_mtf_gu(P,M.Hz);
-
-Pf = (Pf.*Gs) + Gn;
-
-for i = 1:length(Pf)
-    Pf(i) = Pf(i)*Gu(i)*Pf(i);
-end
+% [Gu,Gs,Gn] = spm_csd_mtf_gu(P,M.Hz);
+% 
+% Pf = (Pf.*Gs) + Gn;
+% 
+% for i = 1:length(Pf)
+%     Pf(i) = Pf(i)*Gu(i)*Pf(i);
+% end
 
 y = {exp(P.L)*Pf};
